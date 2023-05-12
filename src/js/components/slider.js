66b86adeba1b4b97b05fe1702d64b87df5fbbe06
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-05-12 21:55:30 +0400

    finish hero-section

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,20 +1,37 @@
 import Swiper, {
   Navigation,
-  Pagination
+  Pagination,
+  Autoplay,
+  EffectFade
 } from 'swiper';
 
 
-Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 
 
-const swiper = new Swiper('.hero__slider', {
-  slidesPerView: 1,
-  navigation: {
-    nextEl: '.hero__next',
-    prevEl: '.hero__prev',
-  },
-  pagination: {
-    el: '.hero__pag',
-    type: 'bullets',
-  },
-});
+const heroSlider = document.querySelector('.hero__slider')
+
+
+if (heroSlider) {
+  const heroSliderSwiper = new Swiper(heroSlider, {
+    slidesPerView: 1,
+    spaceBetween: 150,
+    navigation: {
+      nextEl: '.hero__next',
+      prevEl: '.hero__prev',
+    },
+    pagination: {
+      el: '.hero__pag',
+      type: 'bullets',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    }
+  });
+}
