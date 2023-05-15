062871f2afc915af55f312d0d5b71e83be910ba3
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-05-15 16:55:20 +0400

    added shop-section

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,16 +1,13 @@
 import Swiper, {
   Navigation,
   Pagination,
-  Autoplay,
   EffectFade
 } from 'swiper';
 
 
-Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
-
+Swiper.use([Navigation, Pagination, EffectFade]);
 
 const heroSlider = document.querySelector('.hero__slider')
-
 
 if (heroSlider) {
   const heroSliderSwiper = new Swiper(heroSlider, {
@@ -24,10 +21,6 @@ if (heroSlider) {
       el: '.hero__pag',
       type: 'bullets',
       clickable: true,
-    },
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
     },
     effect: 'fade',
     fadeEffect: {
