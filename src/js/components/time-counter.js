5b8fc9e09d717b68f0d6b91bef24a3d2f009e74d
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-05-17 22:56:28 +0400

    added sale-section

---
+++ src/js/components/time-counter.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const NewTimer = new Date('Jun 17 2023 00:00:00')
+
+  const dayValue = document.querySelector('.sale-timer__day .sale-timer__value')
+  const hrsValue = document.querySelector('.sale-timer__hrs .sale-timer__value')
+  const minsValue = document.querySelector('.sale-timer__mins .sale-timer__value')
+  const secsValue = document.querySelector('.sale-timer__secs .sale-timer__value')
+
+  const dayText = document.querySelector('.sale-timer__day .sale-timer__text')
+  const hrsText = document.querySelector('.sale-timer__hrs .sale-timer__text')
+  const minsText = document.querySelector('.sale-timer__mins .sale-timer__text')
+  const secsText = document.querySelector('.sale-timer__secs .sale-timer__text')
+
+  const timeCount = () => {
+    let now = new Date()
+    let leftUntil = NewTimer - now
+
+    let days = Math.floor(leftUntil / 1000 / 60 / 60 / 24)
+    let hours = Math.floor(leftUntil / 1000 / 60 / 60) % 24
+    let minutes = Math.floor(leftUntil / 1000 / 60) % 60
+    let seconds = Math.floor(leftUntil / 1000) % 60
+
+    dayValue.textContent = days
+    hrsValue.textContent = hours
+    minsValue.textContent = minutes
+    secsValue.textContent = seconds
+  }
+
+  timeCount()
+  setInterval(timeCount, 1000)
+})
