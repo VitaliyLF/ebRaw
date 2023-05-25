7570d7a3dfc2a8607f6a69a7028c8b38b378cccb
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-05-25 21:13:39 +0400

    added adaptive

--- src/js/components/addbtn.js
+++ src/js/components/addbtn.js
@@ -1,5 +1,5 @@
-const shopButtons = document.querySelectorAll('.add-btn--shop');
-const favoritesButtons = document.querySelectorAll('.add-btn--favorites');
+const shopButtons = document.querySelectorAll('.btn--shop');
+const favoritesButtons = document.querySelectorAll('.btn--favorites');
 const valueSpans = document.querySelectorAll('.user-nav__value');
 
 const updateCounter = (counterElement) => {
@@ -7,12 +7,30 @@ const updateCounter = (counterElement) => {
   return () => {
     counterElement.textContent = ++counter;
     counterElement.style.display = 'block';
+
+    const button = counterElement.previousElementSibling;
+
+    if (button && button.classList.contains('btn--shop')) {
+      button.textContent = 'Added';
+      button.classList.add('added');
+    }
   };
 };
 
 const addClickHandler = (buttons, counterElement) => {
   const update = updateCounter(counterElement);
-  buttons.forEach((button) => button.addEventListener('click', update));
+  buttons.forEach((button) => {
+    button.addEventListener('click', update);
+    button.addEventListener('click', () => {
+      if (button.classList.contains('btn--shop')) {
+        button.textContent = 'Added';
+        button.classList.add('added');
+
+      } else if (button.classList.contains('btn--favorites')) {
+        button.classList.add('added-favorites');
+      }
+    });
+  });
 };
 
 addClickHandler(shopButtons, valueSpans[0]);
