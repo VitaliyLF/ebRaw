c1a3cfe05a42b734abe62a30bfd623af8d2e4e6b
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-05-26 13:38:00 +0400

    added scroll

--- src/js/components/smooth-scroll.js
+++ src/js/components/smooth-scroll.js
@@ -1,2 +1,5 @@
 import SmoothScroll from 'smooth-scroll';
-const scroll = new SmoothScroll('a[href*="#"]');
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 300,
+  speedAsDuration: true,
+});
